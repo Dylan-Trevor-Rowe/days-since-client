a85fccd722b39df577bb31485f29e2a0772a3cde
Dylan Rowe <dylanthk@gmail.com>
2020-12-29 15:35:10 -0600

    added comments view

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -15,6 +15,9 @@ import { WellBeingList } from './wellbeing/WellBeingList'
 import { Journal } from './journalentry/Journal'
 import { Articles } from './articles/Articles'
 import { ArticleProvider } from "./articles/ArticleProvider"
+import { ArticleForm } from "./articles/ArticleForm"
+import { Comment } from "./comments/Comments"
+import { CommentProvider } from "./comments/CommentProvider"
 
 export const ApplicationViews = (props) => {
 
@@ -25,55 +28,67 @@ export const ApplicationViews = (props) => {
                     <GoalProvider>
                         <WellBeingProvider>
                             <ArticleProvider>
-                                <NavBar />
-                                <Route exact path="/login"></Route>
-                                <Route exact path="/wellbeing" render={(matchProps) => {
-                                    return <WellBeingList {...props} {...matchProps} />
-                                }} />
-                                <Route path="/goalsform" exact component={GoalsForm} render={(matchProps) => {
-                                    return <GoalsForm {...props} {...matchProps} />
-                                }} />
+                                <CommentProvider>
+
+                                    <NavBar />
+                                    <Route exact path="/login"></Route>
+
+                                    <Route exact path="/wellbeing" render={(matchProps) => {
+                                        return <WellBeingList {...props} {...matchProps} />
+                                    }} />
+                                    <Route exact path="/comments" render={(matchProps) => {
+                                        return <Comment {...props} {...matchProps} />
+                                    }} />
+                                    <Route path="/goalsform" exact component={GoalsForm} render={(matchProps) => {
+                                        return <GoalsForm {...props} {...matchProps} />
+                                    }} />
+
+                                    <Route exact path="/articles" render={(matchProps) => {
+                                        return <Articles {...props} {...matchProps} />
+                                    }} />
+                                    <Route exact path="/journal" render={(matchProps) => {
+                                        return <Journal {...props} {...matchProps} />
+                                    }} />
+                                    <Route path="/goals" exact component={GoalsList} render={(matchProps) => {
+                                        return <GoalsList {...props} {...matchProps} />
+                                    }} />
 
 
-                                <Route exact path="/articles" render={(matchProps) => {
-                                    return <Articles {...props} {...matchProps} />
-                                }} />
-                                <Route exact path="/journal" render={(matchProps) => {
-                                    return <Journal {...props} {...matchProps} />
-                                }} />
-                                <Route path="/goals" exact component={GoalsList} render={(matchProps) => {
-                                    return <GoalsList {...props} {...matchProps} />
-                                }} />
+                                    <Route exact path="/journalform" render={(matchProps) => {
+                                        return <JournalEntryForm {...props} {...matchProps} />
+                                    }} />
+                                    <Route exact path="/articleform" render={(matchProps) => {
+                                        return <ArticleForm {...props} {...matchProps} />
+                                    }} />
 
+                                    <Route exact path="/wellbeingform" render={(matchProps) => {
+                                        return <WellBeingForm {...props} {...matchProps} />
+                                    }} />
+                                    <Route exact path='/wellbeingform/edit/:wellBeingId(\d+)' render={(matchProps) => {
+                                        return <WellBeingForm {...props}
+                                            {...matchProps}
+                                        />
+                                    }} />
 
-                                <Route exact path="/journalform" render={(matchProps) => {
-                                    return <JournalEntryForm {...props} {...matchProps} />
-                                }} />
+                                    <Route exact path='/journalform/edit/:journalId(\d+)' render={(matchProps) => {
+                                        return <JournalEntryForm {...props}
+                                            {...matchProps}
+                                        />
+                                    }} />
+                                    <Route exact path="/goalsform/edit/:goalId(\d+)" render={(matchProps) => {
+                                        return <GoalsForm {...props} {...matchProps} />
+                                    }} />
+                                    <Route exact path="/articles/:articleId(\d+)/comments" render={(matchProps) => {
+                                        return <Comment {...props} {...matchProps} />
+                                    }} />
 
-                                <Route exact path="/wellbeingform" render={(matchProps) => {
-                                    return <WellBeingForm {...props} {...matchProps} />
-                                }} />
-                                <Route exact path='/wellbeingform/edit/:wellBeingId(\d+)' render={(matchProps) => {
-                                    return <WellBeingForm {...props}
-                                        {...matchProps}
-                                    />
-                                }} />
+                                    <ScoreBoardProvider>
+                                        <Route exact path="/">
 
-                                <Route exact path='/journalform/edit/:journalId(\d+)' render={(matchProps) => {
-                                    return <JournalEntryForm {...props}
-                                        {...matchProps}
-                                    />
-                                }} />
-                                <Route exact path="/goalsform/edit/:goalId(\d+)" render={(matchProps) => {
-                                    return <GoalsForm {...props} {...matchProps} />
-                                }} />
-
-                                <ScoreBoardProvider>
-                                    <Route exact path="/">
-
-                                        <ScoreBoardList />
-                                    </Route>
-                                </ScoreBoardProvider>
+                                            <ScoreBoardList />
+                                        </Route>
+                                    </ScoreBoardProvider>
+                                </CommentProvider>
                             </ArticleProvider>
                         </WellBeingProvider>
                     </GoalProvider>
--- src/components/articles/Articles.js
+++ src/components/articles/Articles.js
@@ -1,17 +1,22 @@
 import { useContext, useEffect } from 'react'
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import { ArticleContext } from './ArticleProvider';
 import AddCommentIcon from '@material-ui/icons/AddComment';
+import  Button  from '@material-ui/core/Button';
+import  './Articles.css'
 
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
         flexWrap: 'wrap',
-        '& > *': {
-            margin: theme.spacing(3),
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        margin:10,
+        '& > ': {
+            margin: theme.spacing(2),
             width: theme.spacing(50),
             height: theme.spacing(50),
         },
@@ -23,15 +28,24 @@ export function Articles() {
 
     const { getArticleData, articleData } = useContext(ArticleContext)
 
+   const history = useHistory()
+
+   function handleClick() {
+    history.push("/articleform");
+}
+
     useEffect(() => {
         getArticleData()
     }, [])
 
-    return (
+    return <>
+        <div className="article_button">
+        <Button onClick={handleClick}>post new article</Button>
+        </div>
         <div className={classes.root}>
             {articleData.map((row) => (
-                <div key={row}>
-                    <Paper elevation={5}>
+                <div key={row.id}>
+                    <Paper>
                         <a href={row.link}> article: {row.link}</a>
                         <p> date: {row.date}</p>
                         <p> title: {row.title}</p>
@@ -41,5 +55,5 @@ export function Articles() {
                 </div>
             ))}
         </div>
-    );
+   </>
 }
